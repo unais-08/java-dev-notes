59ec448c0dd83cbd12c01135e9ebe9ca0c9a55fc
Unais Shaikh <unais.shaikh.dev@gmail.com>
2023-09-05 21:17:44 +0530

    java basics

---
+++ Basic.java
@@ -0,0 +1,20 @@
+
+
+public class Basic {
+    public static void main(String[] args) {
+        String name = "Unais";
+        int age = 20;
+        boolean student = true;
+        char surnameStartWith = 'U';
+        double salary = 00.00;
+        float percentage = 83;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(student);
+        System.out.println(surnameStartWith);
+        System.out.println(salary);
+        System.out.println(percentage);
+
+    }
+}
