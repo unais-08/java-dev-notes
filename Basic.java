728cebc8a08f0d46c36f2b4a89fe50ab3dbd29ec
Unais Shaikh <unais.shaikh.dev@gmail.com>
2023-09-06 15:41:43 +0530

    add conditionals

--- Basic.java
+++ Basic.java
@@ -2,7 +2,7 @@
 
 public class Basic {
     public static void main(String[] args) {
-        String name = "Unais";
+        String name = "Unais"; 
         int age = 20;
         boolean student = true;
         char surnameStartWith = 'U';
@@ -16,5 +16,23 @@ public class Basic {
         System.out.println(salary);
         System.out.println(percentage);
 
+        //Conditional Statements in java:(if-then-else)/switch statements
+
+        int driverAge = 16;
+        int applyForElection = 16;
+
+        if(driverAge>=20){
+            System.out.println("You can drive the vehical with legal Licencse");
+        }
+        else if(applyForElection>=18){
+            System.out.println("You are eligible to be a candidate in upcoming elections");
+        }
+        else{
+            System.out.println("you can't drive!!!");
+        }
+
+
     }
 }
+
+
