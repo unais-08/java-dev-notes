80e4b6aac251fd0a4750731f952bbe611e0d0aa5
Unais Shaikh <unais.shaikh.dev@gmail.com>
2023-09-08 16:08:31 +0530

    update foundation

--- Foundation.java
+++ Foundation.java
@@ -1,5 +1,74 @@
+// Define a class called Calculator
+class Calculator {
+    // Define a method called addNumbers that takes two integers (num1 and num2) as parameters
+    public int addNumbers(int num1, int num2) {
+        // Calculate the sum of num1 and num2 and store it in the result variable
+        int result = num1 + num2;
+        // Return the result of the addition
+        return result;
+    }
 
-// Methods in java
+    // Define a method called subNumbers that takes two integers (num1 and num2) as parameters
+    public int subNumbers(int num1, int num2) {
+        // Calculate the difference of num1 and num2 and store it in the result variable
+        int result = num1 - num2;
+        // Return the result of the subtraction
+        return result;
+    }
+
+    // Define a method called mulNumbers that takes two integers (num1 and num2) as parameters
+    public int mulNumbers(int num1, int num2) {
+        // Calculate the product of num1 and num2 and store it in the result variable
+        int result = num1 * num2;
+        // Return the result of the multiplication
+        return result;
+    }
+
+    // Define a method called divNumbers that takes two integers (num1 and num2) as parameters
+    public int divNumbers(int num1, int num2) {
+        // Calculate the division of num1 by num2 and store it in the result variable
+        int result = num1 / num2;
+        // Return the result of the division
+        return result;
+    }
+}
+
+// Define a class called Foundation
 public class Foundation {
-    
+    // Define a method called sayMyfunction with no parameters
+    public void sayMyfunction() {
+        // Print a message to the console
+        System.out.println("I am from foundation class but Calculator class methods doing well");
+    }
+
+    // The main method is the entry point of the program
+    public static void main(String[] args) {
+        // Print "hello" to the console
+        System.out.println("hello");
+
+        // Create an instance of the Calculator class
+        Calculator calculator = new Calculator();
+
+        // Call the addNumbers method of the calculator object with arguments 10 and 10,
+        // and print the result to the console
+        System.out.println(calculator.addNumbers(10, 10));
+
+        // Call the subNumbers method of the calculator object with arguments 10 and 10,
+        // and print the result to the console
+        System.out.println(calculator.subNumbers(10, 10));
+
+        // Call the mulNumbers method of the calculator object with arguments 10 and 10,
+        // and print the result to the console
+        System.out.println(calculator.mulNumbers(10, 10));
+
+        // Call the divNumbers method of the calculator object with arguments 10 and 10,
+        // and print the result to the console
+        System.out.println(calculator.divNumbers(10, 10));
+
+        // Create an instance of the Foundation class
+        Foundation refToSayMyfunction = new Foundation();
+
+        // Call the sayMyfunction method of the Foundation object
+        refToSayMyfunction.sayMyfunction();
+    }
 }
